c703783b40d93cea549a522cc8e4f3a7d8add33d
jeanbrcho <jeancbs18@gmail.com>
2025-06-12 16:07:27 -0300

    componente resutados basico

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Results } from './componets/results/results';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet,Results],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/componets/results/results.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-results',
+  standalone: true,
+  imports: [],
+  templateUrl: './results.html',
+  styleUrl: './results.css'
+})
+export class Results {
+
+  constructor(private router: Router) {}
+
+  playAgain() {
+    // Redirige al inicio del juego
+    this.router.navigate(['/game']);
+  }
+
+   exitGame() {
+    // Redirige a la pantalla de inicio o salir de la app
+    this.router.navigate(['/home']);
+  }
+
+}
