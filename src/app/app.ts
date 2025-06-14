bf9fd8600cacf1c30be21e5072d2373b9bd70569
CLauchanchisan <claudia_casalone@hotmail.com>
2025-06-14 17:48:56 -0300

    Saque footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
-import { FooterComponent } from './footer/footer';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,FooterComponent],
+  imports: [RouterOutlet,Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
