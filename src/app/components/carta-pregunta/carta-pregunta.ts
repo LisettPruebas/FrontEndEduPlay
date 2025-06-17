a5661015d4ef6f31ef75dbb02ab03761f5c66dff
LisettPruebas <lisett.pruebas@gmail.com>
2025-06-17 19:04:33 -0300

    nuevamente agregué las carpetas

--- src/app/components/carta-pregunta/carta-pregunta.ts
+++ src/app/components/carta-pregunta/carta-pregunta.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Pregunta } from '../modelos/pregunta.model';
-import { PuntosResultado } from '../modelos/puntosResultado.model';
+import { Pregunta } from '../../modelos/pregunta.model';
+import { PuntosResultado } from '../../modelos/puntosResultado.model';
 
 
 @Component({
