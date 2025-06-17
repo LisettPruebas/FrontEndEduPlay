f0193e8231c8fd110c0ccdd22b04550653e6eabc
LisettPruebas <lisett.pruebas@gmail.com>
2025-06-17 20:01:44 -0300

    Rutas actualizadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { LoginComponent } from './components/login/login.component';
+import { CartaPreguntaComponent } from './components/carta-pregunta/carta-pregunta';
+import { Results } from './components/results/results';
 
 export const routes: Routes = [
     {path:'',component:Home},
-    {path:"login",component: LoginComponent}
+    {path:"login",component: LoginComponent},
+    {path: "preguntas", component: CartaPreguntaComponent},
+    {path: "resultados", component: Results}
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Results } from './components/results/results';
-import { CartaPreguntaComponent } from "./components/carta-pregunta/carta-pregunta";
 import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header, CartaPreguntaComponent, Results],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
